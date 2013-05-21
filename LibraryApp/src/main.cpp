39903831ef42da15211f40d7c5308068ba74c7b0
TL <Opiframe@Opiframe9>
2013-05-21 10:30:35 +0300

    1st commit

---
+++ LibraryApp/src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Tomi Lammi / Marko Kaikkonen / Matti Tastula
+// Version     : 0.1
+// Copyright   : 
+// Description : Library Application
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hep" << endl;
+	return 0;
+}
